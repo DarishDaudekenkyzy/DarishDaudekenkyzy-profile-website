2d0aa1650887c6d2d71c9daf7e41e43072e11dce
Darish Daudekenkyzy <190103317@stu.sdu.edu.kz>
2021-03-14 03:05:31 +0600

    created form with blog/create view

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Blog</title>
+</head>
+<body>
+    <form method = "POST" action = "{{ route('add-blog') }}">
+        <input type = "text" name = "id" placeholder = "id">
+        <input type = "text" name = "title" placeholder = "title">
+        <input type = "text" name = "body" placeholder = "body">
+</body>
+</html>
